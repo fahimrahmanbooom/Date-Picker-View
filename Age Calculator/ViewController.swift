175767c8413178191459c1e4d999cc3a5bd6b9e3
Fahim Rahman <44144266+fahimrahmanbooom@users.noreply.github.com>
2019-11-15 22:59:17 +0600

    Size fixed for all devices.

--- Age Calculator/ViewController.swift
+++ Age Calculator/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return.lightContent
     }
 
-
+    @IBOutlet weak var dateOfBirthPicker: UIDatePicker!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateOfBirthPicker.setValue(UIColor.white, forKey: "textColor")
+        dateOfBirthPicker.setValue(false, forKey: "highlightsToday")
+        ageLabel.text = "How Old Are You?"
+    }
+    
+    @IBAction func getMyAgeButton(_ sender: UIButton) {
+        age()
+    }
+    
+    func age() {
+        
+        // Formatting the date of birth
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd-MM-yyyy"
+        
+        // getting the current calender
+        let calender = Calendar.current
+        
+        // getting the range from date of birth to the current calender
+        let dateComponent = calender.dateComponents([.day, .month, .year], from: dateOfBirthPicker.date, to: Date())
+        
+        // Checking singular or plural then sending the output
+        var year = "Years"
+        var month = "Months"
+        var day = "Days"
+        
+        if dateComponent.year! < 2 {
+            year = "Year"
+        }
+        if dateComponent.month! < 2 {
+            month = "Month"
+        }
+        if dateComponent.day! < 2 {
+            day = "Day"
+        }
+        ageLabel.text = "\(dateComponent.year!) \(year), \(dateComponent.month!) \(month), \(dateComponent.day!) \(day)"
+    }
 }
-
